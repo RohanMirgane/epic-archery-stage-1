f5ffb0f203260332fa3d4a86fd8433174a72a0f2
RohanMirgane <85567373+RohanMirgane@users.noreply.github.com>
2021-08-22 08:14:11 +0530

    Add files via upload

---
+++ computerBase.js
@@ -0,0 +1,55 @@
+class computerBase {
+    constructor(x,y,width,height){
+        var options = {
+            isStatic:true
+        };
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.image = loadimage("./assets/base2.png");
+        world.add(world,this.body);
+    
+
+
+    }
+display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    Push();
+    translate(pos.x,pos.y);
+    rotate(angel);
+    imageMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
+    Pop();
+}
+      
+    
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
